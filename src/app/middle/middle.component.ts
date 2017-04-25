bdb9b46f2f813d1724b7f1312efce645f236385b
hirose-kaoru <hirose-kaoru@metawater.co.jp>
2017-04-25 14:25:50 +0900

    grid-list追加

--- src/app/middle/middle.component.ts
+++ src/app/middle/middle.component.ts
@@ -17,6 +17,36 @@ export class MiddleComponent implements OnInit {
     {value: 'つけ麺', viewValue: 'つけ麺'},
   ]
 
+  folders = [
+    {
+      name: `写真`,
+      updated: new Date('4/14/17'),
+    },
+    {
+      name: `音楽`,
+      updated: new Date('4/15/17'),
+    },
+    {
+      name: `Excel`,
+      updated: new Date('4/16/17'),
+    },
+  ]
+
+  notes = [
+    {
+      name: 'レシピ表',
+      updated: new Date('12/17/17'),
+    },
+    {
+      name: 'コマンド表',
+      updated: new Date('12/18/17'),
+    },
+    {
+      name: 'アドレス帳',
+      updated: new Date('12/19/17'),
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
--- src/app/under/under.component.ts
+++ src/app/under/under.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./under.component.css']
 })
 export class UnderComponent implements OnInit {
+  tiles = [
+    {text: '1番目', cols: 3, rows: 1, color: 'lightblue'},
+    {text: '2番目', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: '3番目', cols: 1, rows: 1, color: 'lightpink'},
+    {text: '4番目', cols: 2, rows: 1, color: '#ddbdf1'},
+  ]
 
   constructor() { }
 
